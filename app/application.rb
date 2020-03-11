f1969a14641e2bbc80b02e1e7913805ce97eb4f1
Chance Smith <chancesmith97@hotmail.com>
2020-03-11 22:41:47 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,10 @@ class Application
   
   def call(time) 
     resp = Rack::Response.new
+    if Time.to_i =< 12
+      resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
   end
   
   
