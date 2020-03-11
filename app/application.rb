5699e88d420a4f2c8871b2057c4c157b36855997
Chance Smith <chancesmith97@hotmail.com>
2020-03-11 22:46:15 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,9 +3,9 @@ class Application
   def call(env) 
     resp = Rack::Response.new
     if Time.now.hour.between?(0, 11)
-      resp.write "Good Afternoon!"
-    else
       resp.write "Good Morning!"
+    else
+      resp.write "Good Afternoon!"
     end
   end
   
